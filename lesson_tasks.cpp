4b1f3de04d7600def87322832a21f552f4072694
TenshiLenovo <zakan.6000@gmail.com>
2024-10-01 20:43:12 +0800

    Changes after second lesson

--- lesson_tasks.cpp
+++ lesson_tasks.cpp
@@ -1,8 +1,7 @@
 ﻿#define _USE_MATH_DEFINES
 #include <iostream>
-#include <cmath>
+#include <math.h>
 using namespace std;
-
 
 void task_first();
 void task_second();
@@ -11,10 +10,12 @@ void task_forth();
 void task_fifth();
 void task_sixth();
 
+// в некоторых задачах при переводе в int я не прибавляю к значению 0.5, т.к в таком случае у меня не в лучшую сторону
+// меняется точность
 void main()
 {
 	/* Выберите функцию для использования */
-	task_fifth();
+	task_second();
 }
 
 void task_first()
@@ -22,35 +23,37 @@ void task_first()
 	float g, m, s;
 	cout << "Enter the angle value (angle minutes seconds): ";
 	cin >> g >> m >> s;
-	float angle = g + m * g / abs(g) / 60 + s * g / abs(g) / 3600;
+	int sign = ((int)(g) >> 31) & 1;
+	m = (sign ^ 1) * m - sign * m;
+	s = (sign ^ 1) * s - sign * s;
+	float angle = g + m / 60.0 + s / 3600.0;
 	float answer = angle / 180 * M_PI;
 	cout << "Radian value: " << answer;
 }
 
-/*test data:
-* 22 -> 180 30 25.737
-* 45 -> 58 18 36.281
-* -92 -> -231 12 42.175
-*/
 void task_second()
 {
 	float r, alpha, angle, minutes, seconds;
 	cout << "Enter the radian value: ";
 	cin >> r;
+	int sign = ((int)(r) >> 31) & 1;
 	alpha = r * 180.0 / M_PI;
 	angle = (int)(alpha) % 360;
-	minutes = (int)(alpha * 60) % 60 * abs(angle) / angle;
-	seconds = (int)(alpha * 3600) % 60 * abs(angle) / angle;
+	minutes = (sign ^ 1) * (int)(alpha * 60) % 60 - sign * (int)(alpha * 60) % 60;
+	seconds = (sign ^ 1) * (int)(alpha * 3600) % 60 - sign * (int)(alpha * 3600) % 60;
 	cout << "Angle: " << " " << angle << " " << minutes << " " << seconds;
 }
 
 void task_third()
 {
-	float k, p, s, answer;
+	float k, p, s, all_months;
+	int years, months;
 	cout << "Enter the capital, the percentage of profit and the amount of accumulation (number percent number): ";
 	cin >> k >> p >> s;
-	answer = log(s / k) / log(p / 100.0 + 1.0);
-	cout << "You will accumulate the required amount for " << (int)(answer + 0.5) << " months";
+	all_months = log(s / k) / log(p / 100.0 + 1.0);
+	years = (int)(all_months + 0.5) / 12;
+	months = (int)(all_months + 0.5) - years * 12;
+	cout << "You will accumulate the required amount for " << years << " years and " << months << " months";
 }
 
 void task_forth()
@@ -79,8 +82,10 @@ void task_sixth()
 	double r, a;
 	cout << "Enter the radius of the circle and the side of triangle (r a): ";
 	cin >> r >> a;
+	// посчитаю сторону правильного треугольника описанного у окружности
 	double a_the_described_triangle = 6 * r / sqrt(3);
-	double count_of_triangles_in_base_of_triangle = (int)((a - a_the_described_triangle * 2) / 2 / r + 2 + 0.5);
+	// отниму одну сторону описанного треугольника из основания данного треугольника, чтобы убедиться, что в углы треугольнька можно вписать по окружности
+	double count_of_triangles_in_base_of_triangle = (int)((a - a_the_described_triangle - 2 * r) / 2 / r + 2 + 0.5);
 	float answer = (1 + count_of_triangles_in_base_of_triangle) / 2 * count_of_triangles_in_base_of_triangle;
 	cout << "You will be able to cut: " << answer << " circles";
 }
